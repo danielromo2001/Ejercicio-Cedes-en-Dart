d1d16a222b4823e806a1b6fc01dea54d9b0bb58e
Daniel Rodriguez <danielfrodriguez@unicesar.edu.co>
2023-03-03 11:33:19 -0500

    first commit

---
+++ bin/ejercicio_romo.dart
@@ -0,0 +1,40 @@
+void main() {
+  almacen();
+}
+
+void almacen() {
+  dynamic cedesLim = 5;
+  List<dynamic> semana = [
+    "Lunes",
+    "Martes",
+    "Miercoles",
+    "Jueves",
+    "Viernes",
+    "Sabado"
+  ];
+  List<Map<String, dynamic>> informe = [];
+  for (int i = 0; i < cedesLim; i++) {
+    Map<String, dynamic> cedes = {
+      "Cede": i,
+      "Ventas": [2000, 2300, 6700, 5700, 1000, 9000],
+      "Admin": "RoMo",
+      "IVA": [0.19, 0.19, 0.19, 0.19, 0.19, 0.19],
+    };
+    informe.addAll([cedes]);
+    print("|***************************************|");
+    print("|                                       |");
+    print("|          INFORME CEDE # 0${i + 1}            |");
+    print("|                                       |");
+    print("|***************************************|");
+    print("|_______________________________________|");
+    print("|                                       |");
+    print("|Administrador: ${informe[i]["Admin"]}                    |");
+    print("|                                       |");
+    for (int j = 0; j < cedes["Ventas"].length; j++) {
+      print("""|${semana[j]} -> Ventas: ${informe[i]["Ventas"][j]} 
+|IVA: ${(informe[i]["Ventas"][j]) * (informe[i]["IVA"][j])}
+|Ganancias: ${(informe[i]["Ventas"][j]) * (informe[i]["IVA"][j]) - (informe[i]["Ventas"][j])}
+|***************************************|""");
+    }
+  }
+}
---
+++ test/ejercicio_romo_test.dart
@@ -0,0 +1,8 @@
+import 'package:ejercicio_romo/ejercicio_romo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
